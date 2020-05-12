b88e5c57b1da4daf7aa1f746d81d857a61102d3f
Kjintae <wlsxo2112@naver.com>
2020-05-12 17:05:29 +0900

    first commit

---
+++ src/Login/Login.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render() {
+        return (
+            <div>login pasge</div>
+        );
+    }
+};
+
+export default Login;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './Login/Login';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+// render 함수 첫번 째 인자는 처음 보여지고 싶은 컴포넌트
+// 어디에 보여주고 싶은지에 대한 위치가 들어감.
